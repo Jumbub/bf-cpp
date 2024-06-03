4099b3fd2e467e0342f1d0e3686d9ce1fae1c80e
Jamie Bray <jamie@jamiebray.xyz>
2024-06-02 21:16:17 -0700

    Profile a bunch of different file reading solutions

--- src/benchmarks/file.h
+++ src/benchmarks/file.h
@@ -1,7 +1,17 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 namespace file {
 
 char* read_1(const char* filename);
+std::string read_2(const char* filename);
+std::vector<char> read_3(const char* filename);
+std::vector<char> read_4(const char* filename);
+std::string read_5(const char* filename);
+std::string read_6(const char* filename);
+std::string read_7(const char* filename);
+char* read_8(const char* filename);
 
-}
+}  // namespace file
--- src/benchmarks/main.cpp
+++ src/benchmarks/main.cpp
@@ -1,21 +1,71 @@
+#include <iostream>
 #include "benchmark/benchmark.h"
 #include "file.h"
 
 constexpr auto mandelbrot_filename = "tests/mandelbrot.b";
 
-static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string fileContents = file::read_1(mandelbrot_filename);
+static void file_read_1(benchmark::State& state) {
+  for (auto _ : state) {
+    auto output = file::read_1(mandelbrot_filename);
+    benchmark::DoNotOptimize(output);
+  }
 }
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+BENCHMARK(file_read_1);
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
+static void file_read_2(benchmark::State& state) {
+  for (auto _ : state) {
+    auto output = file::read_2(mandelbrot_filename);
+    benchmark::DoNotOptimize(output);
+  }
 }
-BENCHMARK(BM_StringCopy);
+BENCHMARK(file_read_2);
+
+static void file_read_3(benchmark::State& state) {
+  for (auto _ : state) {
+    auto output = file::read_3(mandelbrot_filename);
+    benchmark::DoNotOptimize(output);
+  }
+}
+BENCHMARK(file_read_3);
+
+static void file_read_4(benchmark::State& state) {
+  for (auto _ : state) {
+    auto output = file::read_4(mandelbrot_filename);
+    benchmark::DoNotOptimize(output);
+  }
+}
+BENCHMARK(file_read_4);
+
+static void file_read_5(benchmark::State& state) {
+  for (auto _ : state) {
+    auto output = file::read_5(mandelbrot_filename);
+    benchmark::DoNotOptimize(output);
+  }
+}
+BENCHMARK(file_read_5);
+
+static void file_read_6(benchmark::State& state) {
+  for (auto _ : state) {
+    auto output = file::read_6(mandelbrot_filename);
+    benchmark::DoNotOptimize(output);
+  }
+}
+BENCHMARK(file_read_6);
+
+static void file_read_7(benchmark::State& state) {
+  for (auto _ : state) {
+    auto output = file::read_7(mandelbrot_filename);
+    benchmark::DoNotOptimize(output);
+  }
+}
+BENCHMARK(file_read_7);
+
+static void file_read_8(benchmark::State& state) {
+  for (auto _ : state) {
+    auto output = file::read_8(mandelbrot_filename);
+    benchmark::DoNotOptimize(output);
+  }
+}
+BENCHMARK(file_read_8);
 
 BENCHMARK_MAIN();
