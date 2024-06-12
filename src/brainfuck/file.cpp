4e1390e05ece678cd8eb205fc80bfb5da94b8be6
Jamie Bray <jamie@jamiebray.xyz>
2024-06-11 19:54:07 -0700

    More technically correct file counting, at the cost of performance

--- src/brainfuck/file.cpp
+++ src/brainfuck/file.cpp
@@ -1,16 +1,16 @@
 #include "file.h"
 
+#include <filesystem>
 #include <fstream>
 
 namespace brainfuck {
 
 std::optional<std::vector<char>> read(const std::string filename) {
-  std::ifstream file(filename, std::ios::binary | std::ios::ate);
+  std::ifstream file(filename, std::ios::binary);
   if (!file.good()) {
     return std::nullopt;
   }
-  std::streamsize size = file.tellg();
-  file.seekg(0, std::ios::beg);
+  const auto size = std::filesystem::file_size(filename);
   std::vector<char> buffer(size);
   file.read(buffer.data(), size);
   return buffer;
