c26dec0dc0ff1661c6a151fca66b733d65ea789f
Jamie Bray <jamie@jamiebray.xyz>
2024-06-17 00:46:27 -0700

    5% performance gains by using computed gotos

--- src/brainfuck/execute.cpp
+++ src/brainfuck/execute.cpp
@@ -13,104 +13,129 @@ Error execute(ByteCode instructions) {
 
   // Debug debug{instructions};
 
-next:
-  // debug.trackInstruction(static_cast<size_t>(instruction - instructions.data()));
-  switch (instruction->type) {
-    case DATA_POINTER_ADD:
-      data_pointer += instruction->offset;
-      break;
-    case INSTRUCTION_POINTER_SET_IF_NOT_ZERO: {
-      const auto offset_data_pointer = data_pointer + instruction->offset;
-      if (data[offset_data_pointer] % 256 != 0) {
-        instruction = &instructions[static_cast<size_t>(instruction->value)];
-        goto next;
-      }
-      break;
-    }
-    case DATA_TRANSFER: {
-      const auto offset_data_pointer = data_pointer + instruction->offset;
-      data[offset_data_pointer + instruction->value] += data[offset_data_pointer];
-      data[offset_data_pointer] = 0;
-      break;
-    }
-    case DATA_ADD: {
-      data[data_pointer + instruction->offset] += instruction->value;
-      break;
-    }
-    case DATA_MULTIPLY: {
-      const auto offset_data_pointer = data_pointer + instruction->offset;
-      const auto outputs = instruction->value;
-      for (auto i = 0; i < outputs; i++) {
-        instruction++;
-        data[offset_data_pointer + instruction->offset] += instruction->value * data[offset_data_pointer];
-      }
-      data[offset_data_pointer] = 0;
-      break;
-    }
-    case DATA_SET: {
-      const auto offset_data_pointer = data_pointer + instruction->offset;
-      data[offset_data_pointer] = instruction->value;
-      break;
-    }
-    case INSTRUCTION_POINTER_SET_IF_ZERO: {
-      const auto offset_data_pointer = data_pointer + instruction->offset;
-      if (data[offset_data_pointer] % 256 == 0) {
-        instruction = &instructions[static_cast<size_t>(instruction->value)];
-        goto next;
-      }
-      break;
-    }
-    case DATA_POINTER_ADD_WHILE_NOT_ZERO:
-      while (data[data_pointer + instruction->offset] % 256 != 0) {
-        data_pointer += instruction->value;
-      }
-      break;
-    case DATA_PRINT: {
-      const auto offset_data_pointer = data_pointer + instruction->offset;
-      for (int i = 0; i < instruction->value; i++) {
-        std::cout << static_cast<char>(data[offset_data_pointer] % 256);
-      }
-      break;
-    }
-    case DATA_SET_FROM_INPUT: {
-      const auto offset_data_pointer = data_pointer + instruction->offset;
-      for (int i = 0; i < instruction->value; i++) {
-        char input;
-        std::cin >> std::noskipws >> input;
-        if (!std::cin.eof()) {
-          data[offset_data_pointer] = input;
-        }
-      }
-      break;
-    }
-    case DATA_MULTIPLY_AND_DIVIDE: {
-      const auto offset_data_pointer = data_pointer + instruction->offset;
-      const auto outputs = instruction->value;
-      instruction++;
-      Value iterations = 0;
-      while (data[offset_data_pointer] % 256 != 0) {
-        data[offset_data_pointer] += instruction->value;
-        iterations++;
-      }
+  static void* jumpTable[] = {
+      &&NOOP,
+      &&DONE,
+      &&DATA_ADD,
+      &&DATA_SET,
+      &&DATA_TRANSFER,
+      &&DATA_MULTIPLY,
+      &&DATA_MULTIPLY_AND_DIVIDE,
+      &&DATA_SET_FROM_INPUT,
+      &&DATA_PRINT,
+      &&DATA_POINTER_ADD,
+      &&DATA_POINTER_ADD_WHILE_NOT_ZERO,
+      &&INSTRUCTION_POINTER_SET_IF_ZERO,
+      &&INSTRUCTION_POINTER_SET_IF_NOT_ZERO,
+  };
 
-      for (auto i = 0; i < outputs; i++) {
-        instruction++;
-        data[offset_data_pointer + instruction->offset] += instruction->value * iterations;
-      }
-      break;
+  goto* jumpTable[instruction->type];
+
+DATA_POINTER_ADD:
+  data_pointer += instruction->offset;
+  goto AFTER;
+
+INSTRUCTION_POINTER_SET_IF_NOT_ZERO: {
+  const auto offset_data_pointer = data_pointer + instruction->offset;
+  if (data[offset_data_pointer] % 256 != 0) {
+    instruction = &instructions[static_cast<size_t>(instruction->value)];
+    goto* jumpTable[instruction->type];
+  }
+  goto AFTER;
+}
+
+DATA_TRANSFER: {
+  const auto offset_data_pointer = data_pointer + instruction->offset;
+  data[offset_data_pointer + instruction->value] += data[offset_data_pointer];
+  data[offset_data_pointer] = 0;
+  goto AFTER;
+}
+
+DATA_ADD: {
+  data[data_pointer + instruction->offset] += instruction->value;
+  goto AFTER;
+}
+
+DATA_MULTIPLY: {
+  const auto offset_data_pointer = data_pointer + instruction->offset;
+  const auto outputs = instruction->value;
+  for (auto i = 0; i < outputs; i++) {
+    instruction++;
+    data[offset_data_pointer + instruction->offset] += instruction->value * data[offset_data_pointer];
+  }
+  data[offset_data_pointer] = 0;
+  goto AFTER;
+}
+
+DATA_SET: {
+  const auto offset_data_pointer = data_pointer + instruction->offset;
+  data[offset_data_pointer] = instruction->value;
+  goto AFTER;
+}
+
+INSTRUCTION_POINTER_SET_IF_ZERO: {
+  const auto offset_data_pointer = data_pointer + instruction->offset;
+  if (data[offset_data_pointer] % 256 == 0) {
+    instruction = &instructions[static_cast<size_t>(instruction->value)];
+    goto* jumpTable[instruction->type];
+  }
+  goto AFTER;
+}
+
+DATA_POINTER_ADD_WHILE_NOT_ZERO:
+  while (data[data_pointer + instruction->offset] % 256 != 0) {
+    data_pointer += instruction->value;
+  }
+  goto AFTER;
+
+DATA_PRINT: {
+  const auto offset_data_pointer = data_pointer + instruction->offset;
+  for (int i = 0; i < instruction->value; i++) {
+    std::cout << static_cast<char>(data[offset_data_pointer] % 256);
+  }
+  goto AFTER;
+}
+
+DATA_SET_FROM_INPUT: {
+  const auto offset_data_pointer = data_pointer + instruction->offset;
+  for (int i = 0; i < instruction->value; i++) {
+    char input;
+    std::cin >> std::noskipws >> input;
+    if (!std::cin.eof()) {
+      data[offset_data_pointer] = input;
     }
-    case DONE:
-      // debug.done();
-      return Error::NONE;
-    case NOOP:
-      break;
-    default:
-      __builtin_unreachable();
+  }
+  goto AFTER;
+}
+
+DATA_MULTIPLY_AND_DIVIDE: {
+  const auto offset_data_pointer = data_pointer + instruction->offset;
+  const auto outputs = instruction->value;
+  instruction++;
+  Value iterations = 0;
+  while (data[offset_data_pointer] % 256 != 0) {
+    data[offset_data_pointer] += instruction->value;
+    iterations++;
   }
 
-  instruction++;
+  for (auto i = 0; i < outputs; i++) {
+    instruction++;
+    data[offset_data_pointer + instruction->offset] += instruction->value * iterations;
+  }
+  goto AFTER;
+}
 
-  goto next;
+DONE:
+  // debug.done();
+  return Error::NONE;
+
+NOOP:
+  goto AFTER;
+
+AFTER:
+  // debug.trackInstruction(static_cast<size_t>(instruction - instructions.data()));
+  instruction++;
+  goto* jumpTable[instruction->type];
 };
 
 }  // namespace brainfuck
