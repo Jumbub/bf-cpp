f97f77a366c265b5157683a4b08dc8c02027ff96
Jamie Bray <jamie@jamiebray.xyz>
2024-07-05 13:47:03 -0700

    experiment much slower

--- src/brainfuck/execute.cpp
+++ src/brainfuck/execute.cpp
@@ -1,6 +1,7 @@
 #include "execute.h"
 
 #include <iostream>
+#include <utility>
 
 namespace brainfuck {
 
@@ -20,7 +21,7 @@ void input(int64_t* character, const Value times) {
   }
 }
 
-void setupInstructionAddresses(const Instruction* begin, const Instruction* end, const void* jumpTable[]) {
+void setupInstructionAddresses(const Instruction* begin, const Instruction* end) {
   Instruction* current = const_cast<Instruction*>(begin);
   while (current < end) {
     if (current->type == DATA_POINTER_ADD) {
@@ -32,97 +33,70 @@ void setupInstructionAddresses(const Instruction* begin, const Instruction* end,
     if (current->type == INSTRUCTION_POINTER_SET_IF_NOT_ZERO || current->type == INSTRUCTION_POINTER_SET_IF_ZERO) {
       current->next = const_cast<Instruction*>(begin + current->value);
     }
-    current->jump = const_cast<void*>(jumpTable[current->type]);
     current++;
   }
 }
 
 void execute(const Instruction* begin, const Instruction* end) {
-  const void* jumpTable[] = {
-      &&NEXT,
-      &&DONE,
-      &&DATA_ADD,
-      &&DATA_SET_FROM_INPUT,
-      &&DATA_PRINT,
-      nullptr,
-      &&INSTRUCTION_POINTER_SET_IF_ZERO,
-      &&INSTRUCTION_POINTER_SET_IF_NOT_ZERO,
-      &&DATA_TRANSFER,
-  };
-  setupInstructionAddresses(begin, end, jumpTable);
+  setupInstructionAddresses(begin, end);
 
   int64_t datas[30000] = {0};
   int64_t* data = &datas[0];
   Instruction* instruction = const_cast<Instruction*>(begin);
 
-  data += instruction->move;
-  goto*(instruction->jump);
+  while (true) {
+    data += instruction->move;
+    switch (instruction->type) {
+      case DATA_ADD: {
+        *data += instruction->value;
+      } break;
 
-NEXT: {
-  instruction++;
-  data += instruction->move;
+      case DATA_TRANSFER: {
+        const auto multiplier = (*data & 255);
+        const auto last = instruction->next;
+        while (instruction < last) {
+          instruction++;
+          *(data + instruction->move) += multiplier * instruction->value;
+        }
+        *data = 0;
+      } break;
 
-  goto*(instruction->jump);
-}
+      case INSTRUCTION_POINTER_SET_IF_NOT_ZERO: {
+        const auto while_not_zero = instruction->next == instruction;
+        while (while_not_zero && (*data & 255) != 0) {
+          data += instruction->move;
+        }
+        if ((*data & 255) != 0) {
+          instruction = instruction->next;
+          continue;
+        }
+      } break;
 
-DATA_ADD: {
-  *data += instruction->value;
+      case INSTRUCTION_POINTER_SET_IF_ZERO: {
+        if ((*data & 255) == 0) {
+          instruction = instruction->next;
+          continue;
+        }
+      } break;
 
-  goto NEXT;
-}
+      case DATA_PRINT: {
+        output(*data, instruction->value);
+      } break;
 
-DATA_TRANSFER: {
-  const auto multiplier = (*data & 255);
-  const auto last = instruction->next;
-  while (instruction < last) {
+      case DATA_SET_FROM_INPUT: {
+        input(data, instruction->value);
+      } break;
+
+      case DONE: {
+        return;
+      }
+
+      default: {
+        std::unreachable();
+      }
+    };
     instruction++;
-    *(data + instruction->move) += multiplier * instruction->value;
-  }
-  *data = 0;
-
-  goto NEXT;
+  };
 }
-
-INSTRUCTION_POINTER_SET_IF_NOT_ZERO: {
-  const auto while_not_zero = instruction->next == instruction;
-  while (while_not_zero && (*data & 255) != 0) {
-    data += instruction->move;
-  }
-
-  if ((*data & 255) != 0) {
-    instruction = instruction->next;
-    data += instruction->move;
-
-    goto*(instruction->jump);
-  }
-
-  goto NEXT;
-}
-
-INSTRUCTION_POINTER_SET_IF_ZERO: {
-  if ((*data & 255) == 0) {
-    instruction = instruction->next;
-    data += instruction->move;
-
-    goto*(instruction->jump);
-  }
-
-  goto NEXT;
-}
-
-DATA_PRINT: {
-  output(*data, instruction->value);
-
-  goto NEXT;
-}
-
-DATA_SET_FROM_INPUT: {
-  input(data, instruction->value);
-
-  goto NEXT;
-}
-
-DONE:
-};
 
 }  // namespace brainfuck
