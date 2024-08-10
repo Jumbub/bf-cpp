10d75a47bd120c6181bbd83bc6a1dd54f222c396
Jamie Bray <jamie@jamiebray.xyz>
2024-08-10 15:16:54 -0700

    Theres that slow down im waiting for! actually storing the loop outputs now

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -38,6 +39,30 @@ class InfiniteTape /* ~37 zettabytes in each direction */ {
     }
   }
   std::pair<int64_t, int64_t> index() { return {tapeIndex, cellIndex}; };
+};
+
+struct TrackerIn {
+  std::map<std::pair<int64_t, int64_t>, char> input;
+
+  bool operator==(const TrackerIn& rhs) const {
+    return std::equal(this->input.begin(), this->input.end(), rhs.input.begin(), rhs.input.end());
+  }
+};
+struct TrackerOut {
+  std::map<std::pair<int64_t, int64_t>, char> output;
+  std::vector<char> print;
+  int64_t moved;
+
+  bool operator==(const TrackerOut& rhs) const {
+    return this->moved == rhs.moved &&
+           std::equal(this->print.begin(), this->print.end(), rhs.print.begin(), rhs.print.end()) &&
+           std::equal(this->output.begin(), this->output.end(), rhs.output.begin(), rhs.output.end());
+  }
+};
+struct Tracker : public TrackerIn, public TrackerOut {};
+template <>
+struct std::hash<TrackerIn> {
+  std::size_t operator()(const TrackerIn& s) const noexcept { return s.input.size(); }
 };
 
 int main(int argc, char** argv) {
@@ -117,12 +142,6 @@ int main(int argc, char** argv) {
 
   std::unordered_map<size_t, size_t> executionsOfLoop;
 
-  struct Tracker {
-    std::map<std::pair<int64_t, int64_t>, char> input;
-    std::map<std::pair<int64_t, int64_t>, char> output;
-    std::vector<char> print;
-    int64_t moved;
-  };
   std::stack<Tracker> trackers;
 
   size_t codeIndex = 0;
@@ -161,6 +180,8 @@ int main(int argc, char** argv) {
 
   trackers.push({});
 
+  std::vector<std::unordered_map<TrackerIn, TrackerOut>> loopIdSolves(nextLoopId);
+
   while (codeIndex < code.size()) {
     if (i++ > 500000000) {
       break;
@@ -169,7 +190,7 @@ int main(int argc, char** argv) {
       case '[': {
         if (read() == 0) {
           // skip loop
-          codeIndex = endCodeIndexForStartCodeIndex[codeIndex] + 1;
+          codeIndex = endCodeIndexForStartCodeIndex.at(codeIndex) + 1;
           continue;
         }
 
@@ -181,25 +202,34 @@ int main(int argc, char** argv) {
       case ']': {
         if (read() != 0) {
           // restart loop
-          codeIndex = startCodeIndexForEndCodeIndex[codeIndex] + 1;
+          codeIndex = startCodeIndexForEndCodeIndex.at(codeIndex) + 1;
           continue;
         }
 
+        const auto loopId = loopIdForStartIndex.at(startCodeIndexForEndCodeIndex.at(codeIndex));
         const auto thisTracker = trackers.top();
+
+        // maybe store tracker
+        if (!loopIdSolves.at(loopId).contains(thisTracker)) {
+          loopIdSolves.at(loopId)[thisTracker] = thisTracker;
+        }
+
+        // apply loop innards to parent loop
         trackers.pop();
-        auto& previousTracker = trackers.top();
+        auto& parentTracker = trackers.top();
         for (const auto& [key, value] : thisTracker.input) {
-          if (!previousTracker.input.contains(key)) {
-            previousTracker.input[key] = value;
+          if (!parentTracker.input.contains(key)) {
+            parentTracker.input[key] = value;
           }
         }
         for (const auto& [key, value] : thisTracker.output) {
-          previousTracker.output[key] = value;
+          parentTracker.output[key] = value;
         }
         for (const auto value : thisTracker.print) {
-          previousTracker.print.push_back(value);
+          parentTracker.print.push_back(value);
         }
-        previousTracker.moved += thisTracker.moved;
+        parentTracker.moved += thisTracker.moved;
+
       } break;
       case '+':
         increment(1);
